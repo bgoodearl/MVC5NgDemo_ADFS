d0298727dc58ea479a65a7e60ad2ca188a0f0012
rhurlbut <robert@roberthurlbut.com>
2015-02-04 21:08:52 -0500

    Adding missing js files for music app

---
+++ MVCDemo/app/music/rehearsalDataService.js
@@ -0,0 +1,58 @@
+﻿(function () {
+    'use strict';
+
+    angular
+        .module('appRehearsals')
+        .factory('rehearsalDataService', rehearsalDataService);
+
+    rehearsalDataService.$inject = ['$q', '$http', '$log', 'appConstants'];
+
+    function rehearsalDataService($q, $http, $log, appConstants) {
+        var service = {
+            getRehearsal: getRehearsal,
+            getRehearsals: getRehearsals
+        };
+
+        return service;
+
+        function getRehearsals() {
+            var d = $q.defer();
+            var url = appConstants.rootPath + 'API/Rehearsals';
+            $http.get(url)
+                .success(function(data) {
+                    d.resolve(data);
+                })
+                .error(function(data, status) {
+                    var reason = '';
+                    if (status = 404) {
+                        reason = 'data not found';
+                    } else {
+                        reason = 'get data failed';
+                    }
+                    $log.log('Error', status.toString() + ' ' + reason);
+                    d.reject(reason);
+                });
+            return d.promise;
+        }
+
+        function getRehearsal(id) {
+            var d = $q.defer();
+            var url = appConstants.rootPath + 'API/Rehearsals/' + id;
+            $http.get(url)
+                .success(function(data) {
+                    d.resolve(data);
+                })
+                .error(function(data, status) {
+                    var reason = '';
+                    if (status = 404) {
+                        reason = 'data not found';
+                    } else {
+                        reason = 'get data failed';
+                    }
+                    $log.log('Error', status.toString() + ' ' + reason);
+                    d.reject(reason);
+                });
+            return d.promise;
+        }
+    }
+})();
---
+++ MVCDemo/app/music/rehearsalEdit.js
@@ -0,0 +1,44 @@
+﻿(function () {
+    'use strict';
+
+    angular
+        .module('appRehearsals')
+        .controller('rehearsalEdit', rehearsalEdit);
+
+    rehearsalEdit.$inject = ['$routeParams', 'rehearsalDataService'];
+
+    function rehearsalEdit($routeParams, rehearsalDataService) {
+        /* jshint validthis:true */
+        var vm = this;
+        vm.title = 'rehearsalEdit';
+        vm.id = $routeParams.id;
+        vm.rehearsal = null;
+        vm.message = '';
+        vm.saveEdit = saveEdit;
+
+        activate();
+
+        function activate() {
+            if (vm.id != null) {
+                if (isNaN(vm.id)) {
+                    vm.message = 'invalid id';
+                } else {
+                    rehearsalDataService.getRehearsal(vm.id).then(getUserDataComplete, getUserDataError);
+                }
+            }
+        }
+
+        function getUserDataComplete(data) {
+            vm.rehearsal = data;
+        }
+
+        function getUserDataError(reason) {
+            vm.message = reason;
+            vm.rehearsal = null;
+        }
+
+        function saveEdit() {
+            alert('saveEdit not implemented');
+        }
+    }
+})();
